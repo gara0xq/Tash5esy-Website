54ce4f5685d3917fac916d521af6d779f70bee27
gara0xqm <gara.xq@gmail.com>
2025-01-22 18:23:50 +0200

    garaxq

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -5,12 +5,51 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          color: Colors.red,
+      backgroundColor: Color(0xffF7FCFD),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: width,
+            constraints: BoxConstraints(maxWidth: 1400),
+            child: Column(
+              spacing: 40,
+              children: [
+                Container(
+                  height: 100,
+                  color: Colors.grey.shade300,
+                ),
+                Container(
+                  height: 900,
+                  color: Colors.grey.shade300,
+                  child: Center(
+                    child: Text("Hello, World!"),
+                  ),
+                ),
+                Container(
+                  height: 900,
+                  color: Colors.grey.shade300,
+                ),
+                Container(
+                  height: 900,
+                  color: Colors.grey.shade300,
+                ),
+                Container(
+                  height: 900,
+                  color: Colors.grey.shade300,
+                ),
+                Container(
+                  height: 900,
+                  color: Colors.grey.shade300,
+                ),
+                Container(
+                  height: 400,
+                  color: Colors.grey.shade300,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
